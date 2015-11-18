011132be3c851e8309485655a7521bdc296341e9
Fabrice Nourisson <fabrice@nourisson-pytel.fr>
2015-11-18 20:10:54 +0100

    tests succeed

--- sources/bowling.sh
+++ sources/bowling.sh
@@ -3,10 +3,16 @@ test(){
 }
 game(){
     local roll=$1
-    local score = 0
+    local score=0
+    local frame_index=0
     for loop in ${roll}
     do
-        score=$((score+loop))
+        array=("${array[@]}" "${loop}")
+    done
+    for frame in $(seq 0 9)
+    do
+        score=$((score+array[frame_index]+array[frame_index+1]))
+        frame_index=$((frame_index+2))
     done
     echo ${score}
 }
