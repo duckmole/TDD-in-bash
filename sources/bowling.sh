638c6ae5374912cd8967a4d6e39f0ef1177ced3b
Fabrice Nourisson <fabrice@nourisson-pytel.fr>
2015-11-18 20:18:10 +0100

    tests succeed

--- sources/bowling.sh
+++ sources/bowling.sh
@@ -12,7 +12,7 @@ game(){
     for frame in $(seq 0 9)
     do
         local score_frame=$((array[frame_index]+array[frame_index+1]))
-        if [ $score_frame -eq 10 ]
+        if isSpare $score_frame
         then
             score=$((score+score_frame+array[frame_index+2]))
         else
@@ -21,4 +21,7 @@ game(){
         frame_index=$((frame_index+2))
     done
     echo ${score}
+}
+isSpare(){
+    [ $1 -eq 10 ]
 }
